b94f7ea87f8ac2824ed7c53c1cc38717253a6dae
theonlykingpin <theonlykingpin@outlook.com>
2021-10-19 17:00:31 +0330

    some changes

--- yummy/settings.py
+++ yummy/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 SECRET_KEY = 'skdnfoksjndfoknaajsdhjifgahsdjfhJDSHFJAHSDFAJHSDF'
-ALLOWED_HOSTS = ["*"]
+
 DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
 # Application definition
 
 INSTALLED_APPS = [
